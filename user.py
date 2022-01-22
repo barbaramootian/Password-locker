2e23abc94342e39f93e505d8d81ffc9eae1fcff9
barbaramootian <barbara.mootian@student.moringaschool.com>
2022-01-22 13:19:25 +0300

    added a properties for our objectself

---
+++ user.py
@@ -0,0 +1,10 @@
+class user:
+
+  user_list = []
+
+  def _init_(self, firstname, lastname,username,password) :
+
+   self.firstname = firstname
+   self.lastname = lastname
+   self.username = username
+   self.password = password
